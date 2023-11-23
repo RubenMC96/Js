a2cab20964731d628d1302191c2105173d5fbf26
Rubén Muñoz <rubenmunozcumbreras@gmail.com>
2023-11-23 14:35:20 +0100

    Commit ejercicio 5_9

--- Tema 5/rmc0509/script.js
+++ Tema 5/rmc0509/script.js
@@ -1,14 +1,43 @@
-document.getElementById('registerForm').addEventListener('submit', function(event) {
-    event.preventDefault();
-    var username = document.getElementById('username').value;
-    localStorage.setItem('username', username);
-    alert('Usuario creado con éxito!');
-   });
-   
-   window.addEventListener("load" = function() {
-    var username = localStorage.getItem('username');
-    if (username) {
-      document.getElementById('username').value = username;
+document.getElementById("submit").addEventListener("click", validar, false);
+
+function validarNombre() {
+  let nombre = document.getElementById("nombre");
+
+  if (!nombre.checkValidity()) {
+    if (nombre.validity.valueMissing) {
+      error(nombre, "Introduce un nombre");
     }
-   });
-   
+    if (
+      document
+        .getElementById("nombre")
+        .addEventListener(
+          "patterMismatch",
+          error(nombre, "Solo se admiten letras")
+        )
+    );
+  }
+}
+
+function validar(e) {
+  borrarError();
+  if (validarNombre() && validarContacto() && validarContrasena()) {
+    return true;
+  } else {
+    e.preventDefault();
+    return false;
+  }
+}
+
+function error(e, mensaje) {
+  document.getElementById("aviso").innerHTML = mensaje;
+
+  e.className = "error";
+  e.focus();
+}
+
+function borrarError() {
+  let formulario = document.forms("formulario");
+  for (let i = 0; i < formulario.elements.length; i++) {
+    formulario.elements[i].className = "";
+  }
+}
