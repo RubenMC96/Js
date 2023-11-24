c258b02ee4b2c86930a7c38c3a25b81677c6ed3e
Rubén Muñoz <rubenmunozcumbreras@gmail.com>
2023-11-24 14:35:02 +0100

    Commit ejercicio 5_9

--- Tema 5/rmc0509/script.js
+++ Tema 5/rmc0509/script.js
@@ -1,22 +1,4 @@
-document.getElementById("submit").addEventListener("click", validar, false);
-
-function validarNombre() {
-  let nombre = document.getElementById("nombre");
-
-  if (!nombre.checkValidity()) {
-    if (nombre.validity.valueMissing) {
-      error(nombre, "Introduce un nombre");
-    }
-    if (
-      document
-        .getElementById("nombre")
-        .addEventListener(
-          "patterMismatch",
-          error(nombre, "Solo se admiten letras")
-        )
-    );
-  }
-}
+document.getElementById("enviar").addEventListener("click", validar);
 
 function validar(e) {
   borrarError();
@@ -27,6 +9,39 @@ function validar(e) {
     return false;
   }
 }
+function validarContacto() {
+  let contacto = document.getElementById("contacto");
+  let regExtTlf = /^\d{9}$/;
+  let regExtEmail = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3,4})+$/; //regExt para evaluar un email
+
+  if (regExtTlf.test(contacto) || regExtEmail.test(email)) {
+    return true;
+  } else {
+    error(contacto, "Formato no válido");
+  }
+}
+
+function validarNombre() {
+  let nombre = document.getElementById("nombre");
+
+  if (!nombre.checkValidity()) {
+    if (nombre.validity.valueMissing) {
+      error(nombre, "Introduce un nombre");
+      return false;
+    }
+    if (nombre.validity.patternMismatch) {
+      error(nombre, "Solo se admiten letras");
+      return false;
+    }
+  }
+  return true;
+}
+
+function validarContrasena() {
+  /*Para validar tenemos que evaluar que sean*/
+  let contrasena1 = document.getElementById("contrasena1");
+  let contrasena2 = document.getElementById("contrasena2");
+}
 
 function error(e, mensaje) {
   document.getElementById("aviso").innerHTML = mensaje;
@@ -36,7 +51,7 @@ function error(e, mensaje) {
 }
 
 function borrarError() {
-  let formulario = document.forms("formulario");
+  let formulario = document.forms[0];
   for (let i = 0; i < formulario.elements.length; i++) {
     formulario.elements[i].className = "";
   }
