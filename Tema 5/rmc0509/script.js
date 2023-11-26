42da4fd4be957000d9115a7fac223e83fb52d3a1
= <=>
2023-11-26 21:38:42 +0100

    commit prueba ejercicio 5_9

--- Tema 5/rmc0509/script.js
+++ Tema 5/rmc0509/script.js
@@ -14,10 +14,10 @@ function validarContacto() {
   let regExtTlf = /^\d{9}$/;
   let regExtEmail = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3,4})+$/; //regExt para evaluar un email
 
-  if (regExtTlf.test(contacto) || regExtEmail.test(email)) {
+  if (regExtTlf.test(contacto) || regExtEmail.test(contacto)) {
     return true;
   } else {
-    error(contacto, "Formato no válido");
+    error(contacto, "Formato no válido", "contacto");
   }
 }
 
@@ -25,12 +25,8 @@ function validarNombre() {
   let nombre = document.getElementById("nombre");
 
   if (!nombre.checkValidity()) {
-    if (nombre.validity.valueMissing) {
-      error(nombre, "Introduce un nombre");
-      return false;
-    }
     if (nombre.validity.patternMismatch) {
-      error(nombre, "Solo se admiten letras");
+      error(nombre, "Solo se admiten letras", "nombre");
       return false;
     }
   }
@@ -41,10 +37,40 @@ function validarContrasena() {
   /*Para validar tenemos que evaluar que sean*/
   let contrasena1 = document.getElementById("contrasena1");
   let contrasena2 = document.getElementById("contrasena2");
+
+  if (!contrasena1.checkValidity()) {
+    if (contrasena1.validity.patternMismatch) {
+      error(contrasena1, "No se admiten caracteres especiales", "contrasena");
+      return false;
+    }
+  }
+  if(contrasena1.value.toString() === contrasena2.value.toString()){
+    return true;
+  }
+  else{
+    error(contrasena1, "Las contraseñas no coinciden", "contrasena");
+    return false;
+  }
 }
 
-function error(e, mensaje) {
-  document.getElementById("aviso").innerHTML = mensaje;
+function error(e, mensaje, tipo) {
+
+  if(tipo == "nombre"){
+    document.getElementById("avisoNombre").innerHTML = mensaje;
+    e.style.display = "block";
+  }
+  else if(tipo == "contacto"){
+    document.getElementById("avisoContacto").innerHTML = mensaje;
+    e.style.display = "block";
+  }
+  else{
+    document.getElementById("avisoContrasena").innerHTML = mensaje;
+    e.style.display = "block";
+    let aviso = document.getElementById("aviso");
+    aviso.style.display = "none";
+  }
+
+ 
 
   e.className = "error";
   e.focus();
---
+++ Tema 5/rmc0509_prueba/script.js
@@ -0,0 +1,94 @@
+document.getElementById("enviar").addEventListener("click", validar, false);
+
+function validar(e){
+
+  if(validarNombre() && validarContacto() && validarContrasena()){
+    return true;
+  }
+  else{
+
+    e.preventDefault();
+    return false;
+  }
+}
+
+function validarNombre(){
+  let nombre = document.getElementById("nombre");
+
+  if(!nombre.checkValidity()){
+    if(nombre.validity.pattermismatch){//mismatch da true si encuentra que NO se cumple el patter del HTML
+      
+      error(nombre, "El nombre solo admite letras MAYÚSCULAS y minúsculas", "nombre");
+
+      return false;
+    } 
+  }
+  return true;
+}
+
+function validarContacto(){
+  let contacto = document.getElementById("contacto");
+
+  let regExtTLF = /^\d{9}$/;
+  let regExtEMAIL = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3,4})+$/;
+
+  if(regExtTLF.test(contacto) || regExtEMAIL.test(contacto)){
+    return true;
+  }
+  else {
+    error(contacto,"Ingrese un numero de telefono o correo electronico valido. El telefono debe de contener 9 dígitos y el correo un cominio válido.", "contacto");
+    return false;
+    }
+}
+
+function validarContrasena(){
+  let contrasena1 = document.getElementById("contrasena1");
+  let contrasena2 = document.getElementById("contrasena2");
+
+  if(contrasena1.validity.pattermismatch){
+    
+    error(contrasena1, "Introduce una contraseña válida");
+    
+    return false;
+  }
+
+  if(contrasena1.value.toString() !== contrasena2.value.toString()){
+    
+    error(contrasena1, "Las contraseñas no coinciden", "contrasena");
+
+    return false;
+  }
+
+  return true;
+}
+
+
+
+
+
+function error(elemento, mensaje, tipo){
+
+elemento.className = "error";
+
+if(tipo.toString() == "nombre"){
+
+  document.getElementById("avisoNombre").innerHTML = mensaje;
+
+}
+else if( tipo.toString() == "contacto"){
+
+  document.getElementById("avisoContacto").innerHTML = mensaje;
+
+}
+else{
+
+  document.getElementById("avisoContrasena").innerHTML = mensaje;
+
+}
+
+
+}
+
+function limpiarError(elemento){
+  elemento.className = "";
+}
