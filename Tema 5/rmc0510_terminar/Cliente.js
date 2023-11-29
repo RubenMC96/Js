39f481ba3bee4ba629846a3bd08b9fdc022fe51a
= <=>
2023-11-29 21:20:02 +0100

    commit ejercicio 10

--- Tema 5/rmc0510_terminar/Cliente.js
+++ Tema 5/rmc0510_terminar/Cliente.js
@@ -1,28 +1,52 @@
 export default class Cliente {
-  constructor(documento, fechaNac, clave) {
-    this._documento = documento;
-    this._fechaNac = new Date(fechaNac);
+  // Constructor
+  constructor(documentacion, fecha_nacimiento) {
+    this._documentacion = documentacion;
+    this._fecha_nacimiento = fecha_nacimiento;
+    this._clave = crearClave();
+  }
+
+  // Getters:
+  get documentacion() {
+    return this._documentacion;
+  }
+
+  get fecha_nacimiento() {
+    return this._fecha_nacimiento;
+  }
+
+  get clave() {
+    return this._clave;
+  }
+
+  // Setters:
+  set documentacion(documentacion) {
+    this._documentacion = documentacion;
+  }
+
+  set fecha_nacimiento(fecha_nacimiento) {
+    this._fecha_nacimiento = fecha_nacimiento;
+  }
+
+  set clave(clave) {
     this._clave = clave;
   }
-
-  get documento() {
-    return this._documento;
-  }
-
-  modPrestado(nuevoPrestado) {
-    this.prestado = nuevoPrestado;
-  }
-
-  modCaratula(nuevaCaratula) {
-    this.caratula = nuevaCaratula;
-  }
-
-  showInfo() {
-    let showLocal = this.localizacion;
-    let showPrestado = this.prestado;
-    let showCaratula = this.caratula;
-
-    return `Nombre: ${this.nombre}, Grupo: ${this.grupo}, Año: ${this.anno}, Tipo: ${this.tipo}, Localizacion: ${showLocal}, Prestado: ${showPrestado}, Carátula: ${showCaratula}`;
-  }
 }
-export { Disco };
+
+
+function crearClave() {
+  let numero;
+  let clave = "";
+  
+  while(clave.length != 6){
+    numero = Math.floor(Math.random() * 10);
+
+    if (!clave.includes(numero)) {
+      clave += numero;
+    }
+  }
+  
+  return clave;
+}
+
+export { Cliente };
--- Tema 5/rmc0510_terminar/script.js
+++ Tema 5/rmc0510_terminar/script.js
@@ -1,140 +1,264 @@
-import Cliente from "./Cliente";
+import Cliente from "./Cliente.js";
+
 document.getElementById("enviar").addEventListener("click", validar, false);
 
-let nombreUsuario = localStorage.getItem("nombreUsuario");
-if (nombreUsuario) {
-  document.getElementById("nombre").value = nombreUsuario;
-}
-let datos = [];
+const INICIO = document.getElementById("inicio");
 
-datos.push = new Cliente("12345678Z", "1975/01/01", "123456789");
-datos.push = new Cliente("87654321X", "2000/12/31","987654321");
+const OPCIONES = document.getElementsByName("option");
+const NUMERO_DOCUMENTO = document.getElementById("number");
+const DIA = document.getElementById("dia");
+const MES = document.getElementById("mes");
+const ANO = document.getElementById("anno");
+const RECORDAR = document.getElementById("recordar");
 
-function validar(e){
+const MENSAJE_DOCUMENTO = document.getElementById("mensajeDocumento");
+const MENSAJE_FECHA = document.getElementById("mensajeFecha");
 
-  if(validarDocumento() && validarFechaNac()){
-    
-    localStorage.setItem(
-      "nombreUsuario",
-      document.getElementById("nombre").value
-    ); 
-    alert("El formulario se ha enviado");
-    
-    return true;
+let clientes = [];
+
+// Validaciones
+function validarDocumentos() {
+  let check = false;
+  let validez = false;
+  let respuesta = false;
+
+  for (let i = 0; i < OPCIONES.length; i++) {
+    if (OPCIONES[i].checked) {
+      if (OPCIONES[i].value === "dni") {
+        if (validarDNI(NUMERO_DOCUMENTO)) {
+          validez = true;
+        }
+      } else if (OPCIONES[i].value === "pasaporte") {
+        if (validarPasaporte(NUMERO_DOCUMENTO)) {
+          validez = true;
+        }
+      }
+      check = true;
+    }
   }
-  else{
 
-    e.preventDefault();
+  if (!check) {
+    error(NUMERO_DOCUMENTO, "Tienes que marcar el tipo de documento");
+  }
+
+  if (check && validez) {
+    respuesta = true;
+  }
+
+  return respuesta;
+}
+
+function validarPasaporte(elemento) {
+  const REGEX = /^[A-Z]{3}[0-9]{6}$/;
+
+  if (REGEX.test(elemento.value)) {
+    return true;
+  } else {
+    error(
+      elemento,
+      "El pasaporte está mal. Tiene que tener 3 letras en mayúscula y 6 números"
+    );
     return false;
   }
 }
 
-function validarDocumento(){
-  let documento = document.getElementsByName("opcion");
+function validarDNI(elemento) {
+  const REGEX = /^[0-9]{8}[a-zA-Z]{1}$/;
 
-  if(documento.value == "DNI"){
+  if (REGEX.test(elemento.value)) {
+    return true;
+  } else {
+    error(
+      elemento,
+      "El dni está mal. Tiene que tener ocho números y una letra"
+    );
+    return false;
+  }
+}
 
-    let dni = document.getElementById("dni").value;
+function validarFecha() {
+  let ahora = new Date();
+  let anoActual = ahora.getFullYear();
+  let mesActual = ahora.getMonth() + 1;
+  let diaActual = ahora.getDate();
 
-    for(let i = 0; i<= datos.length; i++){
-
-
-      if()
-
-
-
-
-
-
-
-
-
+  if (!DIA.checkValidity() && !MES.checkValidity() && !ANO.checkValidity()) {
+    if (DIA.validity.valueMissing) {
+      error(DIA, "Tienes que introducir un día");
+    } else if (MES.validity.valueMissing) {
+      error(MES, "Tienes que introducir un mes");
+    } else {
+      error(ANO, "Tienes que introducir un año");
     }
 
+    return false;
   }
 
-  else{
-
-
-
+  if (ANO.value > anoActual - 18) {
+    error(ANO, "No puedes ser menor de edad");
+    return false;
+  } else if (ANO.value == anoActual - 18 && MES.value > mesActual) {
+    error(MES, "No puedes ser menor de edad");
+    return false;
+  } else if (
+    ANO.value == anoActual - 18 &&
+    MES.value == mesActual &&
+    DIA.value > diaActual
+  ) {
+    error(DIA, "No puedes ser menor de edad");
+    return false;
   }
 
+  if (DIA.value < 1 || MES.value < 1 || ANO.value < 1) {
+    error(DIA, "El día, mes o año no pueden ser menores a 1");
+    return false;
+  }
 
-  if(!nombre.checkValidity()){
-    if(nombre.validity.pattermismatch){//mismatch da true si encuentra que NO se cumple el patter del HTML
+  if (MES.value > 12) {
+    error(MES, "El mes no puede ser mayor a 12");
+    return false;
+  } else if (MES.value == 2 && DIA.value > 29) {
+    error(DIA, "Febrero no puede tener más de 29 días");
+    return false;
+  } else if (
+    (MES.value == 4 || MES.value == 6 || MES.value == 9 || MES.value == 11) &&
+    DIA.value > 30
+  ) {
+    error(
+      DIA,
+      "Los meses de abril, junio, septiembre y noviembre no pueden tener más de 30 días"
+    );
+    return false;
+  }
+
+  let fecha = new Date(ANO.value + "/" + MES.value + "/" + DIA.value);
+
+  // devuelve true si el argumento no es un número. Como Date en JavaScript se convierte internamente a un número (el número de milisegundos desde el 1 de enero de 1970), si la fecha es inválida, se convertirá a NaN y isNaN() devolverá true
+  if (isNaN(fecha.getTime())) {
+    error(DIA, "La fecha es inválida");
+    return false;
+  }
+
+  return true;
+}
+
+function error(elemento, mensaje) {
+  let idElemento = elemento.id;
+
+  elemento.className = "error";
+  elemento.focus();
+
+  switch (idElemento) {
+    case "number":
+    case "option":
+      MENSAJE_DOCUMENTO.innerHTML = mensaje;
+      MENSAJE_DOCUMENTO.className = "mensajeError";
+      break;
+    case "day":
+    case "month":
+    case "year":
+      MENSAJE_FECHA.innerHTML = mensaje;
+      MENSAJE_FECHA.className = "mensajeError";
+      break;
+    case "botones":
+      MENSAJE_BOTONES.innerHTML = mensaje;
+      MENSAJE_BOTONES.className = "mensajeError";
+      break;
       
-      error(nombre, "El nombre solo admite letras MAYÚSCULAS y minúsculas", "nombre");
-
-      return false;
-    } 
   }
-  return true;
 }
 
-function validarContacto(){
-  let contacto = document.getElementById("contacto");
+function borrarError() {
+  let formulario = document.forms[0];
+  let mensajes = [MENSAJE_DOCUMENTO, MENSAJE_FECHA];
 
-  let regExtTLF = /^\d{9}$/;
-  let regExtEMAIL = /^w+([.-]?w+)*@w+([.-]?w+)*(.w{2,3,4})+$/;
-
-  if(regExtTLF.test(contacto) || regExtEMAIL.test(contacto)){
-    return true;
+  for (let i = 0; i < formulario.elements.length; i++) {
+    formulario.elements[i].className = "";
   }
-  else {
-    error(contacto,"Ingrese un numero de telefono o correo electronico valido. El telefono debe de contener 9 dígitos y el correo un cominio válido.", "contacto");
-    return false;
+
+  for (let j = 0; j < mensajes.length; j++) {
+    mensajes[j].className = "normal";
+    mensajes[j].innerHTML = "";
+  }
+}
+
+function validar(e) {
+  borrarError();
+  e.preventDefault();
+
+  // Así validamos todo al mismo tiempo en vez de ir uno a uno
+  let documento = validarDocumentos();
+  let fechaValida = validarFecha();
+
+  if (documento && fechaValida) {
+    // Para crear el LocalStorage
+    if (RECORDAR.checked) {
+      // Para almacenar los datos en LocalStorage
+      localStorage.setItem("dni", NUMERO_DOCUMENTO.value);
+      localStorage.setItem(
+        "fechaNacimiento",
+        DIA.value + "/" + MES.value + "/" + ANO.value
+      );
     }
-}
+    // window.location.href = "./clave.html"; -> Así nos redirigiríamos a una nueva página web
 
-function validarContrasena(){
-  let contrasena1 = document.getElementById("contrasena1");
-  let contrasena2 = document.getElementById("contrasena2");
+    // Usamos classList en vez de className porque si no, no podemos
+  
+    // Creación del Cliente
+    if (!verificarCliente(NUMERO_DOCUMENTO.value)) {
+      cliente = new Cliente(
+        NUMERO_DOCUMENTO.value,
+        DIA.value + "/" + MES.value + "/" + ANO.value
+      );
+      clientes.push(cliente);
+      alert("Bienvenido por primera vez");
+    } else {
+      alert("Bienvenido de nuevo");
+    }
 
-  if(contrasena1.validity.pattermismatch){
-    
-    error(contrasena1, "Introduce una contraseña válida");
-    
+
+    return true;
+  } else {
     return false;
   }
+}
 
-  if(contrasena1.value.toString() !== contrasena2.value.toString()){
-    
-    error(contrasena1, "Las contraseñas no coinciden", "contrasena");
+// Para recuperar los datos con el LocalStorage
+window.addEventListener("DOMContentLoaded", (e) => {
+  // Guardamos los valores de dni y fechaNacimiento en las variables
+  let dni = localStorage.getItem("dni");
+  let fechaNacimiento = localStorage.getItem("fechaNacimiento");
 
-    return false;
+  if (dni && fechaNacimiento) { // Si uno de los valores del LocalStorage está vacío devolverá "null", lo cual lo interpretará como un false
+    NUMERO_DOCUMENTO.value = dni;
+
+    let opcionDNI = document.querySelector('input[name="option"][value="dni"]'); // Seleccionamos el botón de "dni" para poder checkearlo
+    opcionDNI.checked = true;
+
+    let fechaPartes = fechaNacimiento.split("/");
+    DIA.value = fechaPartes[0];
+    MES.value = fechaPartes[1];
+    ANO.value = fechaPartes[2];
   }
+});
 
-  return true;
+//----------------------------------------------------------------------------------------------------------------------------------------------------------------------
+// Creación de Cliente
+let cliente;
+
+function verificarCliente(documento) {
+  // Busca si un cliente tiene el mismo número de documento con el método find() -> find() devuelve el primer elemento en el array que cumple con la condición proporcionada que en este caso es la función flecha
+  let clienteExistente = clientes.find(
+    (cliente) => {return cliente.documentacion === documento} // A esta función flecha le tenemos que pasar el cliente como parámetro y este hace una validación ya que llamamos con el getter del atributo _documentación (cliente.documentación -> es el getter no el atributo!!!) al valor que tiene, y miramos si ese valor se corresponde con el que le pasamos (en este caso le pasaríamos NUMERO_DOCUMENTACION.value)
+  );
+
+  return clienteExistente ? clienteExistente.clave : null;
 }
 
-
-
-
-
-function error(elemento, mensaje, tipo){
-
-elemento.className = "error";
-
-if(tipo.toString() == "nombre"){
-
-  document.getElementById("avisoNombre").innerHTML = mensaje;
-
+function buscarClaveCliente(documento) {
+  if (cliente.documentacion === documento) {
+    return cliente.clave;
+  } else {
+    return null;
+  }
 }
-else if( tipo.toString() == "contacto"){
-
-  document.getElementById("avisoContacto").innerHTML = mensaje;
-
-}
-else{
-
-  document.getElementById("avisoContrasena").innerHTML = mensaje;
-
-}
-
-
-}
-
-function limpiarError(elemento){
-  elemento.className = "";
-}
-
