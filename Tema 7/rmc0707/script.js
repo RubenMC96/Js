71e29f42fb98ec780d8f99184a0fbd66c3d74410
Rubén Muñoz <rubenmunozcumbreras@gmail.com>
2024-01-30 14:41:40 +0100

    actualizacion 30/01/2024

--- Tema 7/rmc0707/script.js
+++ Tema 7/rmc0707/script.js
@@ -3,66 +3,81 @@ window.addEventListener("load", iniciar, false);
 let pilotos = [];
 let tablaPilotos = document.getElementById("tablaPilotos");
 
-async function iniciar(){
-
-    try{
-        let response = await fetch("pilotos.json");
-        let json = await response.json();
-        pilotos = [...json];
-        ejecuta();
-    }catch(error){
-        alert("Se ha producido un error");
-    }
-
-    
+async function iniciar() {
+  try {
+    let response = await fetch("pilotos.json");
+    let json = await response.json();
+    pilotos = [...json];
+    // ejecuta();
+  } catch (error) {
+    alert("Se ha producido un error");
+  }
 }
 
-function ejecuta(){
-    let opcion = document.getElementById("opcion").value;
-    let tabla = document.getElementById("tablaPilotos");
-    let fila = document.createElement("tr");
-    let celdaNombre = document.createElement("td");
-    let thNombre = document.createElement("th");
-    let textNombre = document.createTextNode("Nombre");
-    let celdaEquipo = document.createElement("td");
-    let thEquipo = document.createElement("th");
-    let textEquipo = document.createTextNode("Equipo");
-    let celdaNumero = document.createElement("td");
-    let thNumero = document.createElement("th");
-    let textNumero = document.createTextNode("Numero");
-    let celdaNacionalidad = document.createElement("td");
-    let thNacionalidad = document.createElement("th");
-    let textNacionalidad = document.createTextNode("Nacionalidad");
-    let celdaCampeonatos = document.createElement("td");
-    let thCampeonatos = document.createElement("th");
-    let textCampeonatos = document.createTextNode("Campeonatos");
+document.getElementById("enviar").addEventListener("click", ejecuta, false);
 
-    tabla.appendChild(fila);
+function ejecuta() {
+  let opcion = document.getElementById("opcion");
+  let tabla = `<table border="1px"><tr><th>Nombre</th><th>Equipo</th><th>Número</th><th>Nacionalidad</th><th>Campeonatos</th></tr>`;
+  let listaPilotos;
+  switch (opcion) {
+    case "1": //Ordenar por Nombre
+      listaPilotos = pilotos.sort((a, b) => {
+        let nombre1 = a.nombre;
+        let nombre2 = b.nombre;
+        if (nombre1 < nombre2) {
+          return -1;
+        }
+        if (nombre1 > nombre2) {
+          return 1;
+        } else {
+          return 0;
+        }
+      });
+      break;
+    case "2": //Ordenar por equipo
+      listaPilotos = pilotos.sort((a, b) => {
+        let equipo1 = a.equipo;
+        let equipo2 = b.equipo;
+        if (equipo1 < equipo2) {
+          return -1;
+        }
+        if (equipo1 > equipo2) {
+          return 1;
+        } else {
+          return 0;
+        }
+        
+      });
+      let i = 0;
+      listaPilotos.forEach((piloto) => {
+        i++;
+        tabla +=
+          `<tr><td>` +
+          i +
+          `</td><td>${naciolaidadPiloto.nombre}</td><td>${naciolaidadPiloto.equipo}</td><td>${naciolaidadPiloto.numero}</td><td>${naciolaidadPiloto.nacionalidad}</td><td>${naciolaidadPiloto.campeonatos}</td></tr>`;
+      });
 
-    fila.appendChild(celdaNombre);
-    celdaNombre.appendChild(thNombre);
-    thNombre.appendChild(textNombre);
-
-    fila.appendChild(celdaEquipo);
-    celdaEquipo.appendChild(thEquipo);
-    thEquipo.appendChild(textEquipo);
-
-    fila.appendChild(celdaNumero);
-    celdaNumero.appendChild(thNumero);
-    thNumero.appendChild(textNumero);
-
-    fila.appendChild(celdaNacionalidad);
-    celdaNacionalidad.appendChild(thNacionalidad);
-    thNacionalidad.appendChild(textNacionalidad);
-
-    fila.appendChild(celdaCampeonatos);
-    celdaCampeonatos.appendChild(thCampeonatos);
-    thCampeonatos.appendChild(textCampeonatos);
-
-    switch(opcion){
-        case "1":
-            let fila = document.createElement("tr");
-            let celdaNombre = document.createElement("td");
-            let celdaEquipo = document.createElement("td");
-    }
+      tabla += `</table>`;
+      break;
+    case "3": //Filtrar por Nacionalidad
+      let nacionalidad = prompt("Indique la naciolaidad");
+      let naciolaidadPiloto = pilotos.filter(
+        (pilotos) => pilotos.nacionalidad == nacionalidad
+      );
+      tabla += `<td>${naciolaidadPiloto.nombre}</td><td>${naciolaidadPiloto.equipo}</td><td>${naciolaidadPiloto.numero}</td><td>${naciolaidadPiloto.nacionalidad}</td><td>${naciolaidadPiloto.campeonatos}</td></tr></table>`;
+      break;
+  }
 }
+
+let trTabla = document
+  .getElementById("tablaPilotos").getElementsByTagName("tr")
+
+  for(let i = 0; i<= trTabla.length; i ++){
+      trTabla[i].addEventListener("click", destacar, false);
+  }
+function destacar(e) {
+    this.class = "destacado";
+}
+
+
